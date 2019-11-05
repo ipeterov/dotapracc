e498aecd1d662948761d0c2c069dec5d443f0832
Ilya Peterov <ipeterov1@gmail.com>
2019-11-05 23:10:02 +0300

    some stuff

--- dotapracc/settings.py
+++ dotapracc/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'dotapracc.urls'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TEMPLATES = [
     {
