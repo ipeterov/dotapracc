66a2df67fec97e2c50d424cce260ccd09711d8fd
Ilya Peterov <ipeterov1@gmail.com>
2019-11-24 12:09:10 +0300

    Reduce spacing between SelectedHeroes buttons

--- frontend/src/components/SelectedHeroes.jsx
+++ frontend/src/components/SelectedHeroes.jsx
@@ -181,7 +181,7 @@ class SelectedHeroes extends React.Component {
     return (
       <Grid container spacing={2} direction="column">
         <Grid item>
-          <Grid container spacing={2}>
+          <Grid container spacing={1}>
             <Grid item>
               <AddHeroDialog
                 zeroHeroesSelected={zeroHeroesSelected}
