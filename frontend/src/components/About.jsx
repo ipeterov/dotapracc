7bc67f643fd02aff0accb736bb930e7ff79551cd
Ilya Peterov <ipeterov1@gmail.com>
2019-12-07 17:53:34 +0300

    Make About page look more like a landing

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,73 +1,118 @@
 import React from 'react';
-import { Button, Grid, Typography } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 
 
 export default function About() {
   return (
-    <Grid container direction="column" spacing={2}>
-      <Grid item>
-        <Button
-          variant="contained"
-          color="secondary"
-          href="/wizard"
-        >
-          Start practicing
-        </Button>
-      </Grid>
+    <Grid
+      container
+      direction="column"
+      spacing={5}
+    >
+      <Grid
+        item
+        container
+        spacing={1}
+        direction="column"
+      >
+        <Grid item>
+          <Typography variant="h2" style={{ whiteSpace: 'pre-line' }}>
+            Matchmaking platform{'\n'}
+            for 1v1 practice
+          </Typography>
+        </Grid>
 
-      <Grid item>
-        <Typography variant="h5">What is dotapra.cc?</Typography>
-        <Typography>
-          It's a site for finding 1v1 practice partners in Dota 2.
-        </Typography>
-      </Grid>
+        <Grid item>
+          <Typography variant="body1">
+            Sign up with Steam and start practicing in less than a minute
+          </Typography>
+        </Grid>
 
-      <Grid item>
-        <Typography variant="h5">How does it work?</Typography>
-        <Typography>
-          Press "Find match". When a match is found, a dotapra.cc bot will
-          invite you to a lobby, where you can play as many games as you want
-          with your opponent.
-        </Typography>
+        <Grid item>
+          <Button
+            size="large"
+            variant="contained"
+            color="secondary"
+            href="/wizard"
+          >
+            Start practicing
+          </Button>
+        </Grid>
       </Grid>
+      <Grid
+        item
+        container
+        spacing={2}
+      >
+        <Grid item xs={3}>
+          <Card>
+            <CardHeader
+              style={{ paddingBottom: 0 }}
+              title="What is dotapra.cc?"
+            />
+            <CardContent>
+              <Typography>
+                It's a site for finding 1v1 practice partners in Dota 2
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
 
-      <Grid item>
-        <Typography variant="h5">How to set it up?</Typography>
-        <Typography>
-          1. Sign in via Steam
-        </Typography>
-        <Typography>
-          2. Add heroes that you want to practice (and against which heroes)
-        </Typography>
-        <Typography>
-          3. (optional) Write a short message to people who will get matched
-          with you. Probably a good idea to describe the rules you want.
-        </Typography>
-      </Grid>
+        <Grid item xs={3}>
+          <Card>
+            <CardHeader
+              style={{ paddingBottom: 0 }}
+              title="How does it work?"
+            />
+            <CardContent>
+              <Typography>
+                Press "Find match". When a match is found, a dotapra.cc bot will
+                invite you to a lobby, where you can play as many games as you
+                want with your opponent
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
 
-      <Grid item>
-        <Typography variant="h5">Why not just queue 1v1 in dota?</Typography>
-        <Typography>
-          • You will get people who just want to play SF vs SF when you wanna
-          improve your tinker laning, and vice versa. You don't just have to
-          wait for a match, you will sometimes have to tank an abandon. On
-          dotapra.cc you can set what heroes you want to practice and will
-          only get matches with whom you have hero pairs.
-        </Typography>
-        <Typography>
-          • You match with someone only for one game. If you want to have a
-          proper practice session, you will have to invite them after the game
-          yourself. If you're lucky and they don't leave the after-game chat.
-        </Typography>
-      </Grid>
+        <Grid item xs={3}>
+          <Card>
+            <CardHeader
+              style={{ paddingBottom: 0 }}
+              title="Why not just queue 1v1 in dota?"
+            />
+            <CardContent>
+              <Typography>
+                dotapra.cc knows what heroes you want to play and won't get
+                you irrelevant matches. Also, you don't have to communicate that
+                you want to play more games with the same person - it's the
+                default here!
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
 
-      <Grid item>
-        <Typography variant="h5">I want to suggest a feature / report a bug</Typography>
-        <Typography>
-          You can either create an issue on GitHub or discuss it on dotapra.cc
-          discord. I listen to all available feedback and respond to every
-          message.
-        </Typography>
+        <Grid item xs={3}>
+          <Card>
+            <CardHeader
+              style={{ paddingBottom: 0 }}
+              title="I want to suggest a feature / report a bug"
+            />
+            <CardContent>
+              <Typography>
+                You can either create an issue on GitHub or discuss it on
+                dotapra.cc discord. I listen to all available feedback and
+                respond to every message
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </Grid>
   );
